db58d55c3952784ec55681d6660306af34bc79a8
efiso <efisofer26@gmail.com>
2018-06-17 12:51:13 +0300

    connection to service still not working

--- app/src/main/java/com/example/efiso/ex4ap2/ImageService.java
+++ app/src/main/java/com/example/efiso/ex4ap2/ImageService.java
@@ -2,9 +2,18 @@ package com.example.efiso.ex4ap2;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.Image;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
 
 /**
  * Created by efiso on 16/06/2018.
@@ -20,6 +29,33 @@ public class ImageService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Thread thread = new Thread(){
+            public void run(){
+                File pic = null;
+                int imgbyte = 0;
+                try {
+                    InetAddress serverAddress = InetAddress.getByName("10.0.2.2");
+                    Socket socket = new Socket(serverAddress, 8888);
+                    try {
+                        OutputStream output = socket.getOutputStream();
+                        FileInputStream fis = new FileInputStream(pic);
+                        output.write(imgbyte);
+                        output.flush();
+
+                    } catch (Exception e) {
+                        Log.e("TCP", "S Error", e);
+
+                    } finally {
+                        socket.close();
+                    }
+                } catch (Exception e) {
+                    Log.e("TCP", "C: Error", e);
+                }
+            }
+        };
+
+        thread.start();
+
         Toast.makeText(this, getResources().getString(R.string.service_started),
                 Toast.LENGTH_LONG).show();
         return START_STICKY;
@@ -35,5 +71,36 @@ public class ImageService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private class ConnectToService extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... urls) {
+            File pic = null;
+            int imgbyte = 0;
+            try {
+                InetAddress serverAddress = InetAddress.getByName("10.0.2.2");
+                Socket socket = new Socket(serverAddress, 8888);
+                try {
+                    OutputStream output = socket.getOutputStream();
+                    FileInputStream fis = new FileInputStream(pic);
+                    output.write(imgbyte);
+                    output.flush();
+
+                } catch (Exception e) {
+                    Log.e("TCP", "S Error", e);
+
+                } finally {
+                    socket.close();
+                }
+            } catch (Exception e) {
+                Log.e("TCP", "C: Error", e);
+            }
+            return "hello";
+        }
+
+        protected void onPostExecute(int result) {
+            // mImageView.setImageBitmap(result);
+        }
     }
 }
